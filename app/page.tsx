a3ba6a6765d03e4c7037715c71bda81d880b103c
babafemijk <babafemijk@gmail.com>
2024-12-26 20:33:12 +0100

    Build carousel section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,58 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { links } from "@/lib/data";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import AutoScroll from "embla-carousel-auto-scroll";
+
+const logos = [
+  { src: "/carousel-logos/eth.svg", text: "Ethereun", alt: "Ethereum logo" },
+  { src: "/carousel-logos/btc.svg", text: "Bitcoin", alt: "Bitcoin logo" },
+  { src: "/carousel-logos/sol.svg", text: "Solana", alt: "Solana logo" },
+  { src: "/carousel-logos/sui.svg", text: "Sui", alt: "Sui logo" },
+  { src: "/carousel-logos/base.svg", text: "Base", alt: "Base logo" },
+  { src: "/carousel-logos/arb.svg", text: "Arbitrum", alt: "Arbitrum logo" },
+  { src: "/carousel-logos/ava.svg", text: "Avalanche", alt: "Avalanche logo" },
+  { src: "/carousel-logos/opt.svg", text: "Optimism", alt: "Optimism logo" },
+  { src: "/carousel-logos/lisk.svg", text: "Lisk", alt: "Lisk logo" },
+  { src: "/carousel-logos/sei.svg", text: "Sei", alt: "SEI logo" },
+  { src: "/carousel-logos/tron.svg", text: "Tron", alt: "Tron logo" },
+];
 
 export default function Home() {
   return (
     <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
       <Navbar />
       <Header />
+      <Carousel
+        plugins={[AutoScroll({ active: true })]}
+        className="py-28 bg-black"
+      >
+        <CarouselContent className="md:-ml-4">
+          {Array.from({ length: logos.length }, (_, i) => (
+            <CarouselItem
+              key={i}
+              className="lg:ml-14 self-center flex items-center"
+            >
+              <Image
+                src={logos[i].src}
+                alt={logos[i].alt}
+                width={60}
+                height={60}
+                className="mr-4"
+              />
+              <p className="uppercase text-xl font-semibold text-white">
+                {logos[i].text}
+              </p>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+      <Services />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
@@ -61,5 +107,35 @@ function Header() {
         />
       </div>
     </header>
+  );
+}
+
+function Services() {
+  return (
+    <section className="bg-black">
+      <div className="max-w-6xl mx-auto">
+        <h2 className="text-white">What We Offer</h2>
+        <p className="text-white">
+          Iungoo simplifies your Web3 journey by aggregating and categorizing
+          all opportunities in one place
+        </p>
+        <div>
+          <div>
+            <h3>Bounties & Hackathons</h3>
+            <ul>
+              <li>Solve real-world challenges</li>
+              <li>Contribute to projects</li>
+              <li>Join vibrant communities</li>
+            </ul>
+          </div>
+          <Image
+            alt="Screen Image"
+            src="/screen.svg"
+            width={530}
+            height={563}
+          />
+        </div>
+      </div>
+    </section>
   );
 }
